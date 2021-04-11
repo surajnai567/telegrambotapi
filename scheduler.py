126b250e44ec3e52364059d0f71b2d1ecd34dd14
surajnai567 <surajnai567@gmail.com>
2021-04-12 03:43:40 +0530

    organise

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,11 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import threading
 from main import sch
-message_sending_thread = threading.Thread(target=sch.start())
+
+
+def start():
+    sch.start()
+
+
+message_sending_thread = threading.Thread(target=start)
 message_sending_thread.start()
